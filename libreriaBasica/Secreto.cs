5538153744ca604da711c7c3e4d2714e4c6b0c2f
Álvaro Manjón Vara <xmv1001@alu.ubu.es>
2021-11-06 21:48:00 +0100

    Cambios en la clase Secreto

--- libreriaBasica/Secreto.cs
+++ libreriaBasica/Secreto.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Security.Cryptography;
 
 namespace libreriaClases
 {
     public class Secreto
     {
+        private int idSecreto;
         private Usuario usuario;
         private string nombre;
         private string texto;
@@ -15,6 +17,34 @@ namespace libreriaClases
             this.usuario = usuario;
             this.nombre = nombre;
             this.texto = texto;
+        }
+
+        public int IdSecreto
+        {
+            get
+            {
+                return this.idSecreto;
+            }
+            set
+            {
+                this.idSecreto = value;
+            }
+        }
+
+        public Usuario Usuario
+        {
+            get
+            {
+                return this.usuario;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return this.nombre;
+            }
         }
     }
 }
