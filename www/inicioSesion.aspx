d84577b303bf6cd13306f9667b623eafd45e38e4
Álvaro Manjón Vara <xmv1001@alu.ubu.es>
2022-02-04 16:57:17 +0100

    Añadida funcionalidad a la página de inicio de sesión

--- www/inicioSesion.aspx
+++ www/inicioSesion.aspx
@@ -132,7 +132,7 @@
                     <td>&nbsp;</td>
                     <td class="auto-style8">&nbsp;</td>
                     <td class="auto-style3" colspan="3">
-                        <asp:Button ID="btnIniciarSesion" runat="server" Text="Iniciar sesión" Width="199px" />
+                        <asp:Button ID="btnIniciarSesion" runat="server" Text="Iniciar sesión" Width="199px" OnClick="btnIniciarSesion_Click" />
                     </td>
                     <td class="auto-style1">&nbsp;</td>
                     <td>&nbsp;</td>
--- www/inicioSesion.aspx.cs
+++ www/inicioSesion.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Data;
+using libreriaClases;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,14 +11,43 @@ namespace www
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        Usuario uAutenticado = null;
+        DBPruebas db = null;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            db = (DBPruebas)Application["Datos"];
+            if (db == null)
+            {
+                db = new DBPruebas();
+                Application["Datos"] = db;
+            }
 
+            Session["uAutenticado"] = uAutenticado;
+
+            if (!IsPostBack)
+            {
+                this.lblUsuarioError.Text = "";
+                this.lblContraseñaError.Text = "";
+                this.lblInicioError.Text = "";
+            }
         }
 
         protected void btnInicio_Click(object sender, EventArgs e)
         {
             Response.Redirect("inicio.aspx");
+        }
+
+        protected void btnIniciarSesion_Click(object sender, EventArgs e)
+        {
+            if (this.db.ExisteUsuarioEMail(this.tbxUsuario.Text))
+            {
+                uAutenticado = this.db.LeeUsuario(this.tbxUsuario.Text);
+            }
+            else
+            {
+                this.lblUsuarioError.Text = "Este mail no es válido o no existe el usuario";
+            }
         }
     }
 }
