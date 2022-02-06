cf53d2f9c111bc36ed5360d50c53216c8cf049f7
Álvaro Manjón Vara <xmv1001@alu.ubu.es>
2022-02-06 20:49:40 +0100

    Avances en el registro

--- www/registro.aspx.cs
+++ www/registro.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Data;
+using libreriaClases;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,8 +11,20 @@ namespace www
 {
     public partial class WebForm2 : System.Web.UI.Page
     {
+        Usuario uAutenticado = null;
+        DBPruebas db = null;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            db = (DBPruebas)Application["Datos"];
+            if (db == null)
+            {
+                db = new DBPruebas();
+                Application["Datos"] = db;
+            }
+
+            Session["uAutenticado"] = uAutenticado;
+
             if (!IsPostBack)
             {
                 this.lblCorreoError.Text = "";
@@ -22,6 +36,41 @@ namespace www
         protected void btnInicio_Click(object sender, EventArgs e)
         {
             Response.Redirect("inicio.aspx");
+        }
+
+        protected void btnRegistro_Click(object sender, EventArgs e)
+        {
+
+            if (!this.db.ExisteUsuarioEMail(this.tbxCorreo.Text) && this.tbxCorreo.Text != "")
+            {
+                this.lblCorreoError.Text = "";
+                if (this.tbxContraseña.Text != "" && this.tbxContraseña.Text.Equals(this.tbxRepetirContraseña.Text))
+                {
+                    this.lblRepetirContraseñaError.Text = "";
+                    if (this.tbxNombre.Text != "" && this.tbxCorreo.Text != "" 
+                        && this.tbxPreguntaSeguridad1.Text != "" && this.tbxPreguntaSeguridad2.Text != "")
+                    {
+                        this.lblRegistroError.Text = "";
+                        uAutenticado = new Usuario(tbxNombre.Text, tbxCorreo.Text, tbxContraseña.Text, Roles.USUARIO);
+                        db.InsertaUsuario(uAutenticado);
+                        Session["uAutenticado"] = uAutenticado;
+                        Server.Transfer("panel.aspx"); //CAMBIAR
+                    }
+                    else
+                    {
+                        this.lblRegistroError.Text = "Uno o varios de los campos están vacíos";
+                    }
+                    
+                }
+                else
+                {
+                    this.lblRepetirContraseñaError.Text = "Las contraseñas no coinciden";
+                }
+            }
+            else
+            {
+                this.lblCorreoError.Text = "El correo no es válido o ya existe un usuario registrado con este correo";
+            }
         }
     }
 }
