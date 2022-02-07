90948831414c4f4ce39e50883d63ec1fe043d612
Álvaro Manjón Vara <xmv1001@alu.ubu.es>
2022-02-07 19:51:26 +0100

    Página de registro exitoso completada

---
+++ www/registroExitoso.aspx.cs
@@ -0,0 +1,40 @@
+﻿using Data;
+using libreriaClases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace www
+{
+    public partial class WebForm5 : System.Web.UI.Page
+    {
+        Usuario uAutenticado = null;
+        DBPruebas db = null;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            db = (DBPruebas)Application["Datos"];
+            if (db == null)
+            {
+                db = new DBPruebas();
+                Application["Datos"] = db;
+            }
+
+            uAutenticado = (Usuario)Session["uAutenticado"];
+            if (uAutenticado == null)
+            {
+                Server.Transfer("inicioSesion.aspx");
+            }
+
+            lblDescripcionRegistro.Text = "El usuario " + uAutenticado.Correo + " acaba de ser registrado en el sistema, ahora deberá de esperar a que un administrador acepte la solicitud, y a continuación deberá de cambiar la contraseña antes de poder iniciar sesión.";
+        }
+
+        protected void btnInicio_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("inicio.aspx");
+        }
+    }
+}
---
+++ www/registroExitoso.aspx.designer.cs
@@ -0,0 +1,53 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace www
+{
+
+
+    public partial class WebForm5
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control lblTituloRegistro.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTituloRegistro;
+
+        /// <summary>
+        /// Control lblDescripcionRegistro.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblDescripcionRegistro;
+
+        /// <summary>
+        /// Control btnInicio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnInicio;
+    }
+}
