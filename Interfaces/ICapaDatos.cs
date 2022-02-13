5c04da3952519312a2635687f86b57829a1d7653
Álvaro Manjón Vara <xmv1001@alu.ubu.es>
2022-02-13 18:20:01 +0100

    Interfaz actualizada

--- Interfaces/ICapaDatos.cs
+++ Interfaces/ICapaDatos.cs
@@ -7,23 +7,16 @@ namespace Interfaces
 {
     public interface ICapaDatos
     {
-        bool ActualizaSecreto(Secreto secreto);
-        bool ActualizaUsuario(Usuario usuario);
-        bool BorraSecreto(Usuario destinatario, string nombre);
-        bool BorraSecreto(int identificador);
-        bool BorraUsuario(string correo);
-        bool BorraUsuario(int identificador);
-        bool ExisteUsuarioEMail(string correo);
-        bool InsertaSecreto(Secreto secreto);
-        bool InsertaUsuario(Usuario usuario);
-        SortedList<int, Secreto> LeeSecretosUsuario(Usuario usuario);
-        Secreto LeeSecreto(Usuario destinatario, string nombre);
-        Secreto LeeSecreto(int identificador);
         Usuario LeeUsuario(string correo);
-        Usuario LeeUsuario(int identificador);
-        int NumeroSecretos();
-        int NumeroUsuarios();
-        int SiguienteSecreto();
-        int SiguienteUsuario();
+        bool InsertaUsuario(Usuario usuario);
+        Usuario BorraUsuario(string correo);
+        List<Usuario> LeeUsuariosActivos();
+        List<Usuario> LeeUsuariosInactivos();
+        List<Usuario> LeeUsuariosPendientes();
+        SortedList<int, Secreto> LeeSecretosEnviadosUsuario(Usuario usuario);
+        SortedList<int, Secreto> LeeSecretosRecibidosUsuario(Usuario usuario);
+        bool InsertaSecreto(Secreto secreto);
+        Secreto LeeSecreto(int identificador);
+        bool BorraSecreto(int identificador);
     }
 }
